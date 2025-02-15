c0bc67385e7d84712ef8e9baf5ab2cedb45835b1
Chaim-Pollak <ch324id@gmail.com>
2025-02-15 23:40:41 +0200

    refactor LeadershipTeam, HomePage, and ContactForm components for improved structure and functionality; update team member roles and email links, and enhance form handling logic

--- src/components/pages/publicPages/mainPage/LeadershipTeam.jsx
+++ src/components/pages/publicPages/mainPage/LeadershipTeam.jsx
@@ -5,8 +5,8 @@ function LeadershipTeam() {
   const [isHovered, setIsHovered] = useState(false);
   const teamMembers = [
     {
-      name: "Dr. Chaim Pollak",
-      role: "Chief Executive Officer",
+      name: "Chaim Pollak",
+      role: "Team Member",
       description:
         "Expert in business development and strategy with 15 years of experience in the global market",
       email: "ch324id@gmail.com",
@@ -15,8 +15,8 @@ function LeadershipTeam() {
         "https://res.cloudinary.com/dl8slx4ca/image/upload/v1735862135/q0hnskq5l6rlrydtpfpu.png",
     },
     {
-      name: "Dr. Daniel Biton",
-      role: "Chief Technology Officer",
+      name: " Daniel Biton",
+      role: "Team Member",
       description:
         "Leading innovative technological product development with AI specialization",
       email: "biton123654@gmail.com ",
@@ -25,8 +25,8 @@ function LeadershipTeam() {
         "https://res.cloudinary.com/dl8slx4ca/image/upload/v1735862135/pcvrzhdq82xbfo4bk52j.png",
     },
     {
-      name: "Dr. Netanel Malka",
-      role: "Business Development Director",
+      name: " Netanel Malka",
+      role: "Team Member",
       description:
         "Specializing in strategic partnerships and growth in new markets",
       email: "netanel63071@gmail.com",
@@ -35,8 +35,8 @@ function LeadershipTeam() {
         "https://res.cloudinary.com/dl8slx4ca/image/upload/v1735863366/gin2jcoz8nwvokan8gs6.png",
     },
     {
-      name: "Dr. Simcha Klikshtein",
-      role: "Head of Research",
+      name: " Simcha Klikshtein",
+      role: "Team Member",
       description: "Leading research and innovation in advanced technologies",
       email: "sbk88514@gmail.com ",
       phone: "+972 55-676-7159",
@@ -68,25 +68,31 @@ function LeadershipTeam() {
                   transition: "filter 0.3s ease",
                 }}
               />
+
               <h3 className="text-xl font-semibold text-amber-700 text-center mb-2">
                 {member.name}
               </h3>
+
               <h4 className="text-amber-600 font-medium text-center mb-2">
                 {member.role}
               </h4>
-              <p className="text-amber-900 text-sm mb-4 text-center">
+
+              {/* <p className="text-amber-900 text-sm mb-4 text-center">
                 {member.description}
-              </p>
+              </p> */}
+
               <div className="space-y-2">
                 <div className="flex items-center text-amber-600">
                   <Mail className="w-4 h-4 mr-2" />
                   <a
-                    href={`mailto:${member.email}?subject=Contact%20from%20your%20website&body=Hello,%20I%20found%20your%20contact%20on%20the%20website%20and%20would%20like%20to%20know%20more.`}
+                    href={`https://mail.google.com/mail/?view=cm&fs=1&to=${member.email}`}
+                    target="_blank"
                     className="text-sm hover:underline"
                   >
                     {member.email}
                   </a>
                 </div>
+
                 <div className="flex items-center text-amber-600">
                   <Phone className="w-4 h-4 mr-2" />
                   <a
